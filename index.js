09276d4823eafe1dfa8c25774d179d8686102368
Nidiyan Rajendran <nidiyan@gmail.com>
2020-08-14 13:11:12 -0400

    modified index.js

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const app = express();
 const port = 8000;
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.get("/",(req,res) => {res.send("hi")});               // "/" is route
-app.listen(port);
 
+app.get("/", (req,res) => {
+	console.log("hi");
+	res.send("hi")
+});               // "/" is route
 
+app.listen(port, () => {
+	console.log("Listening on port " + port);
+});
