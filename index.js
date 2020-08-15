edb945cb9e88307911f804ccedbcc7b910a38e06
Nidiyan Rajendran <nidiyan@gmail.com>
2020-08-15 12:01:20 -0400

    mongoose added in index.js, gitignore node_modules

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");                        // this is #include-> weird
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
