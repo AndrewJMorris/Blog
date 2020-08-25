a1c124ea4deee17144abdb230adf542c2d724d17
Andrew Morris <ajm1230@gmail.com>
2020-08-25 11:46:24 -0400

    Hi, added home route

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express");                        // this is #include-> weird
+const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
@@ -9,11 +9,20 @@ app.set('view engine', 'ejs');
 
 // Routes
 const createEntry = require('./routes/createEntry');
-const getEntry = require('./routes/getEntries')
+const getEntry = require('./routes/getEntries');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(createEntry);
 app.use(getEntry);
+
+app.get("/", (req, res) => { 
+	res.sendFile(__dirname + "/index.html");
+});
+
+app.get('/cock',(req, res) => {
+	res.write("According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground.");
+	res.send();
+});
 
 mongoose.connect(db.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
 		.then(() => console.log("Connected to MongoDB"))
